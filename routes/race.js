cc9f10d1e9903f7b0b9de8bd494055036a3d3c9c
Deejerz88 <103090951+Deejerz88@users.noreply.github.com>
2022-12-23 12:58:19 -0500

    race route

---
+++ routes/race.js
@@ -0,0 +1,28 @@
+import express from "express";
+import axios from "axios";
+import "dotenv/config";
+
+const router = express.Router();
+
+const key = process.env.RSU_KEY;
+const secret = process.env.RSU_SECRET;
+
+router.get("/", async (req, res) => {
+  const { data } = await axios.get(
+    `https://runsignup.com/rest/races?api_key=${key}&api_secret=${secret}&only_partner_races=T&events=T&format=json`
+  );
+  console.log("data", data);
+  const races = data.races.map((race) => {
+    const { race_id, name, events } = race.race;
+    return {
+      id: race_id,
+      name,
+      type: "race",
+      eventIds: events.map((event) => event.event_id),
+    };
+  });
+  races.unshift({ id: 1127, name: "Team Playmakers", type: "club" });
+  res.json(races);
+});
+
+export default router;
