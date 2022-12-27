5197279bf80e7e2064ada4277f0eebd16436b7ec
Deejerz88 <djmcmillan93@gmail.com>
2022-12-27 02:12:27 -0500

    server.js

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express, { urlencoded } from "express";
 import routes from "./routes/index.js";
+import path from "path";
 import cors from "cors";
+
+const port = process.env.PORT || 5000;
+const directory =
+  process.env.NODE_ENV === "production" ? "./build" : "./public";
 
 const app = express();
 
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.static("public"));
 
 app.use("/", routes);
 
-app.listen(5000, () => {
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(directory, "index.html"));
+});
+
+app.listen(port, () => {
   console.log("Server started on port 5000");
 });
